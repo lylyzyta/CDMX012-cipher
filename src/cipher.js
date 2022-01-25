a3a2367bac97d4604d900ea5957cbdd8e1e40ed4
lylyzyta <lylyzyta@gmail.com>
2022-01-24 23:09:28 -0600

    Mi primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,32 @@
 const cipher = {
-  // ...
-};
+
+  encode: function (a, b, c, d){
+    for (let i = 0; i < a; i++) { // Ciclo para hacerlo tantas veces sea la longitud del msj
+      let text = String(b); //Asegurar que el msjCode sea un string
+      let cambioAsCii = text.charCodeAt(i); // Convierte mi texto en codigo ASCII
+      let formulaMay = (cambioAsCii + Number(c) - 65) % 26 + 65; //Fórmula para ubicar mi letra en ASCII y moverla con el offset
+      d = d.concat(String.fromCharCode(formulaMay)); //console.log(formulaMay);
+      
+      }
+      return d;
+
+  },
+
+  decode: function (a, b, c, d){
+    for (let i = 0; i < a; i++) { // Ciclo para hacerlo tantas veces sea la longitud del msj
+      let text = String(b); //Asegurar que el msjCode sea un string
+      let cambioAsCii = text.charCodeAt(i); // Convierte mi texto en codigo ASCII
+      let formulaMay = (cambioAsCii - Number(c) - 65) % 26 + 65; //Fórmula para ubicar mi letra en ASCII y moverla con el offset
+      if (formulaMay < 65) {
+        formulaMay = formulaMay + 26
+      }
+      d = d.concat(String.fromCharCode(formulaMay)); //console.log(formulaMay);
+      }
+      return d;
+  }
+  };
+  
+
 
 export default cipher;
+  
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+  const buttonCode = document.getElementById("buttonCode");
+  buttonCode.addEventListener("click", letCode);//genera esta funcion cuando haya un click en buttonCode
+  
+  const buttonDecode = document.getElementById("buttonDecode");
+  buttonDecode.addEventListener("click", letDecode);
+  
+    function letCode() {
+      let offset = document.getElementById('inputNum').value; //Toma el valor del desplazamiento
+      let msjCode = document.getElementById('inputCode').value; //Toma el valor del mensaje   
+      let msjLength = msjCode.length; // Obten la longitud del mensaje
+      let txt = "" ;//Recibe el mensaje cifrado 
+      document.getElementById("imprimeLetra").innerHTML = "Tu mensaje es  " + cipher.encode(msjLength, msjCode, offset, txt);
+      
+   }
+    
+     function letDecode() {
+      let offset = document.getElementById('inputNum').value; //Toma el valor del desplazamiento
+      let msjCode = document.getElementById('inputCode').value; //Toma el valor del mensaje   
+      let msjLength = msjCode.length; // Obten la longitud del mensaje
+      let txt = "" ;//Recibe el mensaje cifrado 
+      document.getElementById("imprimeLetra").innerHTML = "Tu mensaje es  " + cipher.decode(msjLength, msjCode, offset, txt);
+    }
+   
